405e81eb19ead7410495e1e41f5721b8bd448be9
Melvin Agustin <857361+magustin@users.noreply.github.com>
2018-02-02 23:48:24 -0500

    build production copy for github page; update package.json; set private var id to public

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -13,7 +13,7 @@ import { ProductService } from '../services/product.service';
 })
 export class ProductDetailsComponent implements OnInit, OnDestroy {
 
-    private id: number;
+    public id: number;
     products: Product[];
     private sub: any;
 
